260c8c921f4a0046fb8fc03777f496ec1bfdbbf9
yujin37 <yj061628@gmail.com>
2025-10-23 20:23:28 +0900

    feat: 자동차 이름 검증 중복 및 허용되지 않은 문자 검증 추가
    허용되는 문자 범위: 0-9, a-z, A-z, 가-힣,_

--- src/main/java/racingcar/RacingCarValidator.java
+++ src/main/java/racingcar/RacingCarValidator.java
@@ -1,12 +1,18 @@
 package racingcar;
 
+import java.util.HashSet;
+import java.util.regex.Pattern;
+
 public class RacingCarValidator {
     private static final String VALIDATE_NAME_BLANK_ERROR_MESSAGE = "일부 이름이 빈칸으로 주어져 있습니다.";
     private static final String VALIDATE_NAME_LENGTH_ERROR_MESSAGE = " 해당 이름 길이가 5글자보다 깁니다.";
+    private static final String VALIDATE_DUPLICATION_ERROR_MESSAGE = " 해당 이름이 이전에 나온 적 있습니다.";
+    private static final String VALIDATE_NOT_ALLOWED_CHAR_ERROR_MESSAGE = " 해당 이름이 이름 규칙에 맞지 않습니다. 이름에는 숫자, 영문자, 한글, _만 나올 수 있습니다.";
     private static final Integer STRICT_LENGTH = 5;
+    private static final Pattern pattern = Pattern.compile("^[0-9a-zA-Z가-힣_]*$");
 
     public static void validateCarName(String[] names){
-
+        HashSet<String> uniqueName = new HashSet<String>();
         for (String name : names){
             if(name.isBlank()) {
                 throw new IllegalArgumentException(VALIDATE_NAME_BLANK_ERROR_MESSAGE);
@@ -14,6 +20,13 @@ public class RacingCarValidator {
             if(name.length()>STRICT_LENGTH){
                 throw  new IllegalArgumentException(name + VALIDATE_NAME_LENGTH_ERROR_MESSAGE);
             }
+            if(uniqueName.contains(name)){
+                throw new IllegalArgumentException(name + VALIDATE_DUPLICATION_ERROR_MESSAGE);
+            }
+            if(!pattern.matcher(name).matches()){
+                throw new IllegalArgumentException(name + VALIDATE_NOT_ALLOWED_CHAR_ERROR_MESSAGE);
+            }
+            uniqueName.add(name);
         }
 
     }
