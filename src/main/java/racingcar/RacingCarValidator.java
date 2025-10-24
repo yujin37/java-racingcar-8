24f58e53b89b65d0675d85dce60d164a1f0fc47a
yujin37 <yj061628@gmail.com>
2025-10-24 12:02:56 +0900

    refactor: 시도 횟수 입력 제한 추가(10만)

--- src/main/java/racingcar/RacingCarValidator.java
+++ src/main/java/racingcar/RacingCarValidator.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import java.math.BigInteger;
 import java.util.HashSet;
 import java.util.regex.Pattern;
 
@@ -11,8 +12,10 @@ public class RacingCarValidator {
     private static final String VALIDATE_TRY_EMPTY_ERROR_MESSAGE = "시도 횟수 입력이 비어있습니다.";
     private static final String VALIDATE_TRY_NOT_INTEGER_ERROR_MESSAGE = "시도 횟수가 허용되지 않은 형태로 입력되었습니다. 반드시 자연수로 입력되어야 합니다.";
     private static final String VALIDATE_TRY_NOT_ZERO_AND_NEGATIVE_ERROR_MESSAGE = "시도 횟수는 반드시 0보다 큰 자연수로 입력되어야 합니다.";
+    private static final String VALIDATE_TRY_ALLOWED_ARRANGE_ERROR_MESSAGE = "시도 횟수 범위가 초과되었습니다. 시도 횟수는 최대 10만 이내로만 제한합니다.";
     private static final Integer STRICT_LENGTH = 5;
     private static final Pattern pattern = Pattern.compile("^[0-9a-zA-Z가-힣_]*$");
+    private static final BigInteger MAX_TRYCOUNT = BigInteger.valueOf(100000);
 
     public static void validateCarName(String[] names){
         HashSet<String> uniqueName = new HashSet<String>();
@@ -38,16 +41,20 @@ public class RacingCarValidator {
             throw new IllegalArgumentException(VALIDATE_TRY_EMPTY_ERROR_MESSAGE);
         }
 
-        Integer intCount;
+        BigInteger intCount;
 
         try {
-            intCount = Integer.parseInt(count);
+            intCount = new BigInteger(count);
         } catch (Exception e) {
             throw new IllegalArgumentException(VALIDATE_TRY_NOT_INTEGER_ERROR_MESSAGE);
         }
 
-        if(intCount <= 0) {
+        if(intCount.compareTo(BigInteger.ZERO) <= 0) {
             throw new IllegalArgumentException(VALIDATE_TRY_NOT_ZERO_AND_NEGATIVE_ERROR_MESSAGE);
+        }
+
+        if(intCount.compareTo(MAX_TRYCOUNT) > 0) {
+            throw new IllegalArgumentException(VALIDATE_TRY_ALLOWED_ARRANGE_ERROR_MESSAGE);
         }
 
         //System.out.println(intCount);
