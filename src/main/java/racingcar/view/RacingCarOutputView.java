119e6dbd306c598f9eabc75f8092d9ba84e52a4d
yujin37 <yj061628@gmail.com>
2025-10-25 12:21:43 +0900

    style: 최종 우승자 공동 출력 시 , 뒤에 띄어쓰기 적용

--- src/main/java/racingcar/view/RacingCarOutputView.java
+++ src/main/java/racingcar/view/RacingCarOutputView.java
@@ -7,13 +7,15 @@ public class RacingCarOutputView {
     public static void runMessage() {
         System.out.println("실행 결과");
     }
+
     public static void perMemberResult(List<Car> cars) {
-        for(Car car : cars) {
+        for (Car car : cars) {
             System.out.println(car.getCarName() + " : " + car.getDashMove());
         }
         System.out.println();
     }
+
     public static void finalWinner(List<String> winners) {
-        System.out.println("최종 우승자 : " + String.join(",", winners));
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
     }
 }
