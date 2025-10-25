5e5ed447551e3c4a852af84bca43fa6e1e8c19cb
yujin37 <yj061628@gmail.com>
2025-10-25 12:46:36 +0900

    refactor: Race 클래스 내 game 메서드 indent 2이내로 변경

--- src/main/java/racingcar/model/RacingCarRace.java
+++ src/main/java/racingcar/model/RacingCarRace.java
@@ -5,32 +5,45 @@ import java.util.List;
 import racingcar.view.RacingCarOutputView;
 
 public class RacingCarRace {
+    public void moveCars(List<Car> cars, Dice dice) {
+        for (Car car : cars) {
+            moveCarIfPossible(car, dice);
+        }
+    }
+
+    public void moveCarIfPossible(Car car, Dice dice) {
+        if (dice.isMoveable()) {
+            car.moveCar();
+        }
+    }
+
+    public List<String> moveMaxCars(int maxMove, List<String> maxName, Car car) {
+        if (maxMove < car.moveStatus) {
+            maxName = new ArrayList<>();
+            maxName.add(car.getCarName());
+        } else if (maxMove == car.moveStatus) {
+            maxName.add(car.getCarName());
+        }
+        return maxName;
+    }
+
     public void game(String[] carName, String tryCount) {
         int intTryCount = Integer.parseInt(tryCount);
 
         List<Car> carMoves = new ArrayList<>();
         RacingCarOutputView.runMessage();
-        for(String name : carName) {
+        for (String name : carName) {
             carMoves.add(new Car(name.trim()));
         }
         Dice dice = new Dice();
-        for(int i=0;i<intTryCount;i++){
-            for (Car car : carMoves) {
-                if (dice.isMoveable()) {
-                    car.moveCar();
-                }
-            }
+        for (int i = 0; i < intTryCount; i++) {
+            moveCars(carMoves, dice);
             RacingCarOutputView.perMemberResult(carMoves);
         }
         int maxMove = 0;
         List<String> maxName = new ArrayList<>();
-        for(Car car : carMoves) {
-            if(maxMove < car.moveStatus) {
-                maxMove = car.moveStatus;
-                maxName = new ArrayList<>();
-                maxName.add(car.getCarName());
-            } else if(maxMove == car.moveStatus)
-                maxName.add(car.getCarName());
+        for (Car car : carMoves) {
+            maxName = moveMaxCars(maxMove, maxName, car);
         }
         RacingCarOutputView.finalWinner(maxName);
     }
