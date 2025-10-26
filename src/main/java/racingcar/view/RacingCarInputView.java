9fef6c28ac85e1c8d3f552084e3d4b168000cce9
yujin37 <yj061628@gmail.com>
2025-10-27 01:07:34 +0900

    refactor: inputView, outputView 출력 메시지 상수로 이동

--- src/main/java/racingcar/view/RacingCarInputView.java
+++ src/main/java/racingcar/view/RacingCarInputView.java
@@ -3,14 +3,16 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class RacingCarInputView {
+    private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_TRY_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?";
 
-    public static String inputCarName(){
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    public static String inputCarName() {
+        System.out.println(INPUT_CAR_NAME_MESSAGE);
         return Console.readLine();
     }
 
-    public static String inputTryCount(){
-        System.out.println("시도할 횟수는 몇 회인가요?");
+    public static String inputTryCount() {
+        System.out.println(INPUT_TRY_COUNT_MESSAGE);
         return Console.readLine();
     }
 
--- src/main/java/racingcar/view/RacingCarOutputView.java
+++ src/main/java/racingcar/view/RacingCarOutputView.java
@@ -4,19 +4,25 @@ import java.util.List;
 import racingcar.model.Car;
 
 public class RacingCarOutputView {
+
+    private static final String RUN_RESULT_MESSAGE = "실행결과";
+    private static final String PER_RESULT_SEPERATOR = " : ";
+    private static final String FINAL_WINNER_MESSAGE = "최종 우승자 : ";
+    private static final String FINAL_WINNER_SEPERATOR = ", ";
+
     public static void runMessage() {
         System.out.println();
-        System.out.println("실행 결과");
+        System.out.println(RUN_RESULT_MESSAGE);
     }
 
     public static void perMemberResult(List<Car> cars) {
         for (Car car : cars) {
-            System.out.println(car.getCarName() + " : " + car.getDashMove());
+            System.out.println(car.getCarName() + PER_RESULT_SEPERATOR + car.getDashMove());
         }
         System.out.println();
     }
 
     public static void finalWinner(List<String> winners) {
-        System.out.println("최종 우승자 : " + String.join(", ", winners));
+        System.out.println(FINAL_WINNER_MESSAGE + String.join(FINAL_WINNER_SEPERATOR, winners));
     }
 }
