de1db4e7b939f8f724f70fb2747bc9ee24373888
yujin37 <yj061628@gmail.com>
2025-10-26 20:28:19 +0900

    refactor: 주사위 최소, 최대값 상수로 변경

--- src/main/java/racingcar/model/Dice.java
+++ src/main/java/racingcar/model/Dice.java
@@ -5,10 +5,13 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class Dice {
 
     private final int threadhold = 4;
+    private final int minNum = 0;
+    private final int maxNum = 9;
 
     private int roll() {
-        return Randoms.pickNumberInRange(0,9);
+        return Randoms.pickNumberInRange(minNum, maxNum);
     }
+
     public boolean isMoveable() {
         return roll() >= threadhold;
     }
